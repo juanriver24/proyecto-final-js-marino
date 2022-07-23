3ea477d81793f1cf811e0c3e9eb8e666e79fab20
unknown <juanfmarino@hotmail.com>
2022-07-23 00:49:08 -0300

    personalizando index

--- js/main.js
+++ js/main.js
@@ -1,30 +1,6 @@
-// ARRAY OBJETOS (PRODUCTOS)
-
-class prenda {
-    constructor (nombre,costo,precio) {
-        this.nombre = nombre;
-        this.costo = costo;
-        this.precio = precio ;
-    }   
-
-    aumentoAutomatico () {
-        this.costo = this.costo * 1.25;
-        this.precio = this.precio * 1.25;
-    }
-
-}
-
-const prendas = []
-
-prendas.push (new prenda ("Remera River", 2200, 4500)) ;
-prendas.push (new prenda ("Remera Nike", 1200, 3000));
-prendas.push (new prenda ("Remera Puma", 800,200));
-
-console.log (prendas)
-
 // Calcular costo de envío
 
-let envio = prompt ("Ingrese a cuantos km se encuentra de CABA")
+ let envio =    prompt ("Ingrese a cuantos km se encuentra de CABA")
 
 const precioEnvio1 = 350 ;
 
@@ -34,9 +10,56 @@ function costoEnvio (){
 
     if (distanciaEnvio <= 100){
         console.log ("El costo de envio es de $" + costoEnvio1);
-    } else {
+    } 
+    
+    if (distanciaEnvio >100) {
         console.log ("El costo de envio es de $" + costoEnvio2);
     }   
 } 
 
+// ARRAY OBJETOS (PRODUCTOS)
+
+class prenda {
+    constructor (nombre,costo,precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.costo = costo;
+        this.precio = precio ;
+    }  
+}
+
+const prendas = []
+
+prendas.push (new prenda (id= 1,"Remera River", 2200, 4500)) ;
+prendas.push (new prenda (id= 2,"Remera Nike", 1200, 3000));
+prendas.push (new prenda (id= 3,"Remera Puma", 800,200));
+
+console.log (prendas)
+
+
 // Carrito
+
+const carritoCompras = []
+
+function agregarAlCarrito (prenda){
+    carritoCompras.push (prenda)
+    console.log (carritoCompras)
+}
+
+agregarAlCarrito (prendas)
+
+
+function eliminarDelCarrito (prenda) {
+
+    const prendaAEliminar = carritoCompras.findIndex ((prenda) = prenda.id = id)
+
+    if (prendaAEliminar !== -1)
+
+
+  carritoCompras.splice (prendaAEliminar,1);
+}
+
+eliminarDelCarrito (1)
+
+console.log (carritoCompras)
+
